3ba08f8a515228a851abb4fdf49095039dfa4cc1
kazurego7 <kazurego7@gmail.com>
2019-05-27 11:59:07 +0900

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bingo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
